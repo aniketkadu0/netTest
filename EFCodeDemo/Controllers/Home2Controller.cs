5220fda5577f18b83d1d868636d89dae8e314b54
aniketkadu0 <108684448+aniketkadu0@users.noreply.github.com>
2022-09-07 12:47:21 +0530

    Add files via upload

---
+++ EFCodeDemo/Controllers/Home2Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeDemo.Models;
+
+namespace EFCodeDemo.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Insert(Student std)
+        {
+            DacDbContext db = new DacDbContext();
+            db.students.Add(std);
+            db.SaveChanges();
+            return Content("Successfully Inserted");
+        }
+    }
+}
---
+++ EFCodeDemo/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace EFCodeDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int StudID { get; set; }
+        public string StudName { get; set; }
+        public string City { get; set; }
+    }
+    public class DacDbContext : DbContext
+    {
+        public DacDbContext() : base(@"data source=ACTSB14;initial catalog=DACDB;user id=sa;password=cdacacts")
+        {
+        }
+        public DbSet<Student> students { get; set; }
+         
+    }
+}
